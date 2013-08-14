ce2c42ad3c3c70f487a7f2033096e6db20a7afb8
Fab Mackojc <fab.mackojc@gmail.com>
2013-08-14 08:38:09 -0700

    Configure routes for a read-only API for stores

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 BenandjerrysApi::Application.routes.draw do
+  scope "/api" do
+    scope "/v1" do
+      resources :stores, only: [:index, :show]
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +45,7 @@ BenandjerrysApi::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
