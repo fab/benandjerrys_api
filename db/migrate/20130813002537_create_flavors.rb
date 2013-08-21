fa4a9a08cb3d7cdb21e17b7841d4d63702281b86
Fab Mackojc <fab.mackojc@gmail.com>
2013-08-20 21:21:34 -0700

    Add additional fields to flavor model

--- db/migrate/20130813002537_create_flavors.rb
+++ db/migrate/20130813002537_create_flavors.rb
@@ -2,6 +2,8 @@ class CreateFlavors < ActiveRecord::Migration
   def change
     create_table :flavors do |t|
       t.string :name
+      t.string :description
+      t.string :ingredients
       t.string :img_url
 
       t.timestamps
