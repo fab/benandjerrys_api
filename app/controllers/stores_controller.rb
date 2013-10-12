d827ac3fc34a69c8643b02c8f217bc6e04ee0cce
Fab Mackojc <fab.mackojc@gmail.com>
2013-10-11 21:46:05 -0700

    Create a method for stores that renders the address string

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,13 +2,14 @@ class StoresController < ApplicationController
   def index
     @stores = Store.all
 
-    render json: @stores.to_json(only: [:id, :name, :address])
+    render json: @stores.to_json(only: [:id, :name], methods: :address)
   end
 
   def show
     @store = Store.find(params[:id])
 
-    render json: @store.to_json(only: [:id, :name, :address],
+    render json: @store.to_json(only: [:id, :name],
+                                methods: :address,
                                 include: {flavors: {only: [:id, :name]}})
   end
 end
