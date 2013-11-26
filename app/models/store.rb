529de769d60089c0ca8621a2dfd4f3e353ef7fff
Fab Mackojc <fab.mackojc@gmail.com>
2013-11-26 00:44:30 -0800

    Custom format for street string to render numbered streets properly
    * Titleize helper was doing 2 Nd, 5 Th instead of 2nd and 5th etc.

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,6 +2,10 @@ class Store < ActiveRecord::Base
   has_and_belongs_to_many :flavors
 
   def address
-    "#{street.titleize}, #{city.titleize}, #{state} #{zipcode}"
+    "#{format_street}, #{city.titleize}, #{state} #{zipcode}"
+  end
+
+  def format_street
+    street.split(' ').map(&:capitalize).join(' ')
   end
 end
