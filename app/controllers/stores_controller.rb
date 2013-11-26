59704be095b33f6bbcdaa16a255fd4d7dba0d54d
Fab Mackojc <fab.mackojc@gmail.com>
2013-11-25 23:56:34 -0800

    Can now filter stores by one specific flavor

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,9 +1,11 @@
 class StoresController < ApplicationController
   def index
-    @stores = Store.where('name like ?', "%#{params[:name].try(:upcase)}%")
-                   .where('city like ?', "%#{params[:city].try(:upcase)}%")
-                   .where('state like ?', "%#{params[:state].try(:upcase)}%")
-                   .where('zipcode like ?', "%#{params[:zipcode]}%")
+    @stores = Store.where('stores.name like ?', "%#{params[:name].try(:upcase)}%")
+                   .where('stores.city like ?', "%#{params[:city].try(:upcase)}%")
+                   .where('stores.state like ?', "%#{params[:state].try(:upcase)}%")
+                   .where('stores.zipcode like ?', "%#{params[:zipcode]}%")
+                   .includes(:flavors)
+                   .where('flavors.name like ?', "%#{params[:flavor]}%")
 
     render json: @stores, each_serializer: CompactStoreSerializer
   end
